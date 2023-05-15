08d9f8349ed6c6dbba05f7bd923ed29e7b3cef31
Askar Karasaev <askar.karasaev@gmail.com>
2023-05-15 20:48:33 +0900

    section-3 lesson-54

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,21 @@ import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepPurple,
+                Colors.deepOrange,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
