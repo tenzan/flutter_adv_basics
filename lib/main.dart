c4b9e85d3277f2e5b3a74cdb66d40d6c31227444
Karasawa <Askar.Karasawa@dentsplysirona.com>
2023-05-22 15:33:45 +0900

    section-3 lesson-57

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
-import 'package:adv_basics/start_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.deepPurple,
-                Colors.deepOrange,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+  runApp(const Quiz()
   );
 }
