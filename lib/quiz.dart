f1ec16fd2218de62d6b1f1cad32bec57048e2378
Askar Karasaev <askar.karasaev@gmail.com>
2023-06-08 10:16:29 +0900

    section-3 lesson-58

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
 
@@ -11,9 +12,17 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
@@ -26,7 +35,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
